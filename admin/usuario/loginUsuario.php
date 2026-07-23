c997d660c762ff10bb4b0c55cfbb77b4d9f7c2d6
chachadograu697 <charllesgabriel655@gmail.com>
2026-07-23 17:28:09 -0300

    Co-authored-by: João Fiuza <joaofiuza02@users.noreply.github.com>

--- admin/usuario/loginUsuario.php
+++ admin/usuario/loginUsuario.php
@@ -1,26 +1,33 @@
 <?php
 session_start();
-include_once __DIR__ . '/../../includes/conexao.php';
+ require_once dirname(__DIR__, 2) . '/includes/config.php';
+
+require_once ROOT_PATH . '/includes/conexao.php';
 
 if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $loginUsuario = $_POST['loginUsuario'];
-    $senhaUsuario = $_POST['senhaUsuario'];
+    $loginUsuario = trim($_POST['loginUsuario'] ?? '');
+    $senhaUsuario = $_POST['senhaUsuario'] ?? '';
 
     $sql = "SELECT * FROM usuario WHERE loginUsuario = '$loginUsuario'";
 
     $resultado = mysqli_query($conexao, $sql);
 
-    if(mysqli_num_rows($resultado)>0){
+    if (!$resultado) {
+    die(mysqli_error($conexao));
+    }
 
+    if(mysqli_num_rows($resultado)>0){
       $dados = mysqli_fetch_assoc($resultado);
 
+    
       if(password_verify($senhaUsuario, $dados['senhaUsuario'])){
 
       $_SESSION['idUsuario']= $dados['idUsuario'];
       $_SESSION['nomeUsuario']= $dados['nomeUsuario'];
       $_SESSION['loginUsuario']= $dados['loginUsuario'];
 
-      header('location: /../../index.php');
+      header("Location: " . BASE_URL . "index.php");
+      exit;
       }else {
       echo "Usuário ou senha incorretos!";
       }
