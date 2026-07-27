0a33f4e653a4df78ceeffe57c12f33f446fd110b
Willians Jr <williansjr008@gmail.com>
2026-07-27 17:23:37 -0300

    upd

--- includes/header.php
+++ includes/header.php
@@ -44,7 +44,7 @@ include_once __DIR__ . '/config.php';
 
             <!-- BARRA DE BUSCA -->
     
-<form class="d-flex mx-auto w-50 form formularioBusca" action="pesquisarProduto.php"role="search">
+<form class="d-flex mx-auto w-50 form formularioBusca" action="<?= BASE_URL ?>pesquisarProduto.php"role="search">
 
     <div class="position-relative flex-grow-1 me-2">
         <span class="material-symbols-outlined position-absolute top-50 end-0 translate-middle-y me-3 text-secondary">
