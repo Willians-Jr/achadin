ac1271b198a1a4b0550a098f58bd5bd0d937aa5e
João Pedro <020307jp@gmail.com>
2026-08-03 17:33:06 -0300

    fix: remove duplicate link for "Lojas" in header.php

--- includes/header.php
+++ includes/header.php
@@ -78,7 +78,7 @@ $_SESSION['nivel'] = $dado['nivel'];
 
                 <li class="nav-item">
                     <a class="nav-link link-light" href="<?= BASE_URL ?>admin/loja/lojas.php">Lojas</a>
-                    <a class="nav-link link-light" href="<?= BASE_URL ?>admin/loja/lojas.php">Lojas</a>
+                    
                 </li>
 
                 <li class="nav-item">
