de415fc05d8371554f3de1e60f63b058536340f2
João Pedro <020307jp@gmail.com>
2026-08-03 16:27:16 -0300

    style: remove redundant list styles and clean up header.php structure

--- includes/header.php
+++ includes/header.php
@@ -84,15 +84,15 @@ $_SESSION['nivel'] = $dado['nivel'];
                     <a class="nav-link link-light" href="<?= BASE_URL ?>comoFunciona.php">Como Funciona</a>
                 </li>
 
-                <li class="favoritosIcon nav-item">
                     <?php if (!isset($_SESSION['idUsuario'])) { ?>
+                <li class="favoritosIcon nav-item">
                     <a href="<?= BASE_URL ?>admin/usuario/loginUsuario.php" class="btn btn-outline-light" role="button" aria-pressed="true">
-                        <span class="material-symbols-outlined">
-                    login
-                    </span>
-                    <span>Login</span>
-                        </a>
+                        <span class="material-symbols-outlined">login</span>
+                        <span>Login</span>
+                    </a>
+                </li>
                     <?php } else{?>
+                    <li class="favoritosIcon nav-item">
                         <div class="dropdown">
 
     <button class="btn btn-outline-light dropdown-toggle d-flex align-items-center gap-2"
@@ -104,13 +104,11 @@ $_SESSION['nivel'] = $dado['nivel'];
     <img src="<?php echo BASE_URL . 'assets/UPLOAD/' . htmlspecialchars($imgUsuario); ?>"alt="Foto do usuário"class="rounded-circle"width="32"
                 height="32" style="object-fit:cover;"><span>Perfil</span>
 <?php }else{ ?>
-    <i class="bi bi-person-circle"></i>
-    
-
-
-            
+    <i class="bi bi-person-circle"></i>    
         <span>Perfil</span>
- <?php }}?> 
+ <?php }
+ }
+ ?> 
     </button>
 
     <ul class="dropdown-menu dropdown-menu-end shadow">
